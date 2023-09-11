9cdca3a4c38e4a468e3362e84985d83ddada672b
Rexzooly Kai Black <rexzooly@gmail.com>
2023-09-12 00:02:27 +0100

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -4,12 +4,12 @@
  * Ajax Caller
  * 
  * @package WaterCooler Chat
- * @author Jo„o Ferreira <jflei@sapo.pt>
- * @copyright (c) 2018, Jo„o Ferreira
+ * @author Jo√£o Ferreira <jflei@sapo.pt>
+ * @copyright (c) 2018, Jo√£o Ferreira
  * @since 1.1
  */
 
-include __DIR__.'/wcchat.class.php';
+include __DIR__.'/cpchat.class.php';
 $chat = new WcChat();
 $chat->ajax();
 
