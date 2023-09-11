bb07f626b1eaa69b82ffc6ff898f89e1b8018e16
Rexzooly Kai Black <rexzooly@gmail.com>
2023-09-12 00:07:46 +0100

    Update index.embedded.php

--- index.embedded.php
+++ index.embedded.php
@@ -4,7 +4,7 @@
 	<head>
 		<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
 		<!-- Change paths to match your server's -->
-		<LINK rel="stylesheet" href="themes/simple_blue/style.css" type="text/css">
+		<LINK rel="stylesheet" href="themes/simple_coffee/style.css" type="text/css">
 		<script type="text/javascript" src="script.js"></script>
 		<title>Chat</title>
 	</head>
@@ -12,9 +12,14 @@
 		<h1 style="text-align: center; font-weight: normal">Chat</h1>
 		<?php
 
-			include 'wcchat.class.php';    # Change this to match your server's path to "wcchat.class.php"
+			include 'cpchat.class.php';    # Change this to match your server's path to "wcchat.class.php"
 			$chat = new WcChat();
-                  echo $chat->printIndex('EMBED');
+                  	echo $chat->printIndex('EMBED');
+			/*
+			include 'cpchat.class.php';    # Change this to match your server's path to "wcchat.class.php"
+			$chat = new CpChat();
+                  	echo $chat->printIndex('EMBED');
+			*/
 		?>	
 	</body>
 </html>
