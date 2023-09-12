f20e5287439c06e418ec843cf33304623a863cc7
Rexzooly Kai Black <rexzooly@gmail.com>
2023-09-12 01:08:08 +0100

    Update index.embedded.php

--- index.embedded.php
+++ index.embedded.php
@@ -1,4 +1,19 @@
-<?php ob_start(); ?>
+<?php 
+/**
+ * Embedded
+ * 
+ * @package CoffeePot Chat
+ * @author Rexzooly Black <>
+ * @copyright (c) 2023, rexzooly.co.uk
+ * @since 1.5
+ # ==========================================
+ * @Original package WaterCooler Chat
+ * @Original author Joao Ferreira <jflei@sapo.pt>
+ * @Original copyright (c) 2018, Joao Ferreira
+ * @since 1.1
+ */
+
+ob_start(); ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 	<head>
