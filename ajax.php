65664484ed7a5cb250b08cd425781eee548bf640
Rexzooly Kai Black <rexzooly@gmail.com>
2023-09-12 01:09:17 +0100

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,11 +1,15 @@
 <?php
-
 /**
  * Ajax Caller
  * 
- * @package WaterCooler Chat
- * @author João Ferreira <jflei@sapo.pt>
- * @copyright (c) 2018, João Ferreira
+ * @package CoffeePot Chat
+ * @author Rexzooly Black <>
+ * @copyright (c) 2023, rexzooly.co.uk
+ * @since 1.5
+ # ==========================================
+ * @Original package WaterCooler Chat
+ * @Original author Joao Ferreira <jflei@sapo.pt>
+ * @Original copyright (c) 2018, Joao Ferreira
  * @since 1.1
  */
 
